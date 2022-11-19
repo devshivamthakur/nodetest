9f19fd75dc6d395653aa0ab82066da3d7f52dc1e
shivam <64252021+itsprogrammingwithme@users.noreply.github.com>
2022-11-19 14:45:48 +0530

    DF

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 const express = require('express')
 const app = express()
-const port = 5050
+const port = process.env.PORT ||5000;
 const Gun = require('gun')
 
 app.use(Gun.serve)
+
 
 const server = app.listen(port, () => {
   console.log(`Gun server running on port ${port}🔥`)
